d7fc158339c3fc6e7bb9168339a53263f6ce5391
David Jagoe <david@jagoe.eu>
2026-08-18 16:37:52 -0700

    WIP trying to prevent rearrange of graph after ccomps.

--- field_wiring/script/build.sh
+++ field_wiring/script/build.sh
@@ -9,24 +9,34 @@
 # - Remove hardcoding of field_wiring/ path
 # - Order of connectors & sheets based on gv filename sequence
 # - Template the titleblock for sheet number
-#
+# - Use trap to ensure cleanup
 
 BUILD_DIR=$(mktemp -d)
+SHEETS_DIR=$BUILD_DIR
+
+# SHEETS_DIR="$BUILD_DIR/sheets"
+# mkdir "$SHEETS_DIR"
 
 
 # Run wireviz on the YAML file to produce the monolithic graphviz "gv" file (containing entire field wiring)
 PYTHONPATH="$PYTHONPATH:$PWD/src" python -m wireviz.wv_cli -o "$BUILD_DIR" -f g field_wiring/Field_Wiring.yml
 
-# Since we can't print the entire thing on a single sheet, split it up.
-# ccomps does magic to split parts into individual gv files.
-# Each will be called "sheet_n.gv in the dir specified
-ccomps -x -o "$BUILD_DIR/sheet.gv" "$BUILD_DIR/Field_Wiring.gv"
+# Since we can't print the entire thing on a single sheet, split it
+# up.  ccomps (Connected Component Filter) does the magic to split
+# parts into individual gv files.  Each will be called "sheet_n.gv in
+# the dir specified
+ccomps -x -o "$SHEETS_DIR/sheet.gv" "$BUILD_DIR/Field_Wiring.gv"
 
-for gv_file in "$BUILD_DIR/"*.gv
+for gv_file in "$SHEETS_DIR"/*.gv
 do
 
     # For each xyz.gv, next command creates xyz.gv.ps alongside, one for each sheet.
     dot -O -Tps -Gpage=17,11 -Gmargin=0.5 -Gsize=12,7.5 -Gcenter=true "$gv_file"
+
+    # neato -n2 -Tps -Gpage=17,11 -Gmargin=0.5 -Gsize=16,10 -Gcenter=true \
+    #       -o build/sheet01.ps build/sh.gv
+
+
 done
 
 
@@ -34,7 +44,7 @@ done
 
 # Create the A3 PDF
 gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dDEVICEWIDTHPOINTS=1190 -dDEVICEHEIGHTPOINTS=842 -dFIXEDMEDIA \
-   -sOutputFile=field_wiring/field_wiring.pdf "$BUILD_DIR/"*.ps
+   -sOutputFile=field_wiring/field_wiring.pdf "$SHEETS_DIR"/*.ps
 
 
 # Overlay the titleblock on the created PDF.
